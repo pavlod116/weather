8ab7b74723ca70265982ab60f1beb5d088523362
macbook <pavlo.daluk@asta.mobi>
2023-07-27 09:48:25 +0200

    add file constants

---
+++ lib/ui/constants/AppColors.dart
@@ -0,0 +1,5 @@
+
+import 'package:flutter/material.dart';
+
+const Color background = Colors.white;
+const Color black = Colors.black;
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather/ui/constants/AppColors.dart';
+import 'package:weather/ui/constants/AppString.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,10 +14,21 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 100,
-          height: 50,
-          color: Colors.pink,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(weather,
+              style: TextStyle(fontSize: 20, color: black),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 100,
+              height: 50,
+              color: Colors.pink,
+            ),
+          ],
         ),
       ),
     );
