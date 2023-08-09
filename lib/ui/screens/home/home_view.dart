e6d3817c4c1ce05637eecbaa54b1a8dbe6462d74
Pablo <pavlo.daluk@asta.mobi>
2023-08-09 13:03:24 +0200

    I added a weather list that scrolls horizontally, then I will make a code fix to make it clean.

--- lib/ui/screens/home/home_view.dart
+++ lib/ui/screens/home/home_view.dart
@@ -4,6 +4,7 @@ import 'package:weather/ui/constants/AppColors.dart';
 import 'package:weather/ui/constants/AppImages.dart';
 import 'package:weather/ui/constants/AppString.dart';
 import 'package:weather/ui/screens/home/home_view_model.dart';
+import 'package:weather/ui/widgets/custom_text_form_field.dart';
 import 'package:weather/ui/widgets/icon_widget.dart';
 
 class HomeView extends StatefulWidget {
@@ -45,38 +46,322 @@ class _HomeViewState extends State<HomeView> {
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
-            child: TextFormField(
-              cursorColor: AppColor.black,
-              decoration: const InputDecoration(
-                prefixIcon: ImageIcon(
-                  AssetImage(
-                    AppImage.searchIcon,
+          const CustomTextFormField(),
+          const Padding(
+            padding: EdgeInsets.only(left: 30, top: 20),
+            child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  AppStrings.nextWeek,
+                  style: TextStyle(
+                    color: AppColor.textColor,
+                    fontSize: 21,
+                    fontWeight: FontWeight.w700,
                   ),
-                  color: AppColor.iconSearch,
-                ),
-                hintText: AppStrings.hintTextSearch,
-                filled: true,
-                fillColor: AppColor.searchBackground,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: AppColor.searchBackground),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(14),
+                )),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                      child: Stack(
+                        children:  [
+                          const Align(
+                            alignment: Alignment.topCenter,
+                            child:
+                           Padding(
+                              padding: EdgeInsets.only(top: 4),
+                              child: Text(
+                                '18°C',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                    color: AppColor.background),
+                              )),),
+                      Align(
+                        alignment: Alignment.center,
+                        child:
+                        Image.asset(AppImage.sunLittleRain),),
+                      const Align(
+                        alignment: Alignment.bottomCenter,
+                        child:
+                        Padding(
+                              padding: EdgeInsets.only(bottom: 4),
+                              child: Text(
+                                '06:00',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 10,
+                                    color: AppColor.background),
+                              )),
+                      ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: AppColor.searchBackground),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(14),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                      child: Stack(
+                        children:  [
+                          const Align(
+                            alignment: Alignment.topCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(top: 4),
+                                child: Text(
+                                  '18°C',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                      color: AppColor.background),
+                                )),),
+                          Align(
+                            alignment: Alignment.center,
+                            child:
+                            Image.asset(AppImage.sunLittleRain),),
+                          const Align(
+                            alignment: Alignment.bottomCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(bottom: 4),
+                                child: Text(
+                                  '06:00',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: AppColor.background),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: AppColor.searchBackground),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(14),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                      child: Stack(
+                        children:  [
+                          const Align(
+                            alignment: Alignment.topCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(top: 4),
+                                child: Text(
+                                  '18°C',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                      color: AppColor.background),
+                                )),),
+                          Align(
+                            alignment: Alignment.center,
+                            child:
+                            Image.asset(AppImage.sunLittleRain),),
+                          const Align(
+                            alignment: Alignment.bottomCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(bottom: 4),
+                                child: Text(
+                                  '06:00',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: AppColor.background),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                      child: Stack(
+                        children:  [
+                          const Align(
+                            alignment: Alignment.topCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(top: 4),
+                                child: Text(
+                                  '18°C',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                      color: AppColor.background),
+                                )),),
+                          Align(
+                            alignment: Alignment.center,
+                            child:
+                            Image.asset(AppImage.sunLittleRain),),
+                          const Align(
+                            alignment: Alignment.bottomCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(bottom: 4),
+                                child: Text(
+                                  '06:00',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: AppColor.background),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                      child: Stack(
+                        children:  [
+                          const Align(
+                            alignment: Alignment.topCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(top: 4),
+                                child: Text(
+                                  '18°C',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                      color: AppColor.background),
+                                )),),
+                          Align(
+                            alignment: Alignment.center,
+                            child:
+                            Image.asset(AppImage.sunLittleRain),),
+                          const Align(
+                            alignment: Alignment.bottomCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(bottom: 4),
+                                child: Text(
+                                  '06:00',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: AppColor.background),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                      child: Stack(
+                        children:  [
+                          const Align(
+                            alignment: Alignment.topCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(top: 4),
+                                child: Text(
+                                  '18°C',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                      color: AppColor.background),
+                                )),),
+                          Align(
+                            alignment: Alignment.center,
+                            child:
+                            Image.asset(AppImage.sunLittleRain),),
+                          const Align(
+                            alignment: Alignment.bottomCenter,
+                            child:
+                            Padding(
+                                padding: EdgeInsets.only(bottom: 4),
+                                child: Text(
+                                  '06:00',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 10,
+                                      color: AppColor.background),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: AppColor.cartBackground,
+                          borderRadius: BorderRadius.all(Radius.circular(17))),
+                      width: 62,
+                      height: 88,
+                    ),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: AppColor.cartBackground,
+                        borderRadius: BorderRadius.all(Radius.circular(17))),
+                    width: 62,
+                    height: 88,
+                  ),
+                ],
               ),
             ),
           ),
