8db734429fbfb24ad6afa5d96468d624b865fd86
Pablo <pavlo.daluk@asta.mobi>
2023-08-29 11:35:57 +0200

    create screens settings.

--- lib/ui/screens/settings/setting_view_model.dart
+++ lib/ui/screens/settings/setting_view_model.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:weather/ui/screens/message/message_view.dart';
 
-class SettingViewModel extends ChangeNotifier {}
+class SettingViewModel extends ChangeNotifier {
+  void navigationPushMessage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const MessageView()),
+    );
+  }
+
+  void navigationPushPop(BuildContext context) {
+    Navigator.pop(context);
+  }
+}
